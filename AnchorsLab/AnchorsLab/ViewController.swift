ecc0f7b81dd76cca10bf26974c8090269f9e7669
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-18 17:22:53 +0200

    adding secondary label and button

--- AnchorsLab/AnchorsLab/ViewController.swift
+++ AnchorsLab/AnchorsLab/ViewController.swift
@@ -20,14 +20,25 @@ class ViewController: UIViewController {
         let upperLeftLabel = makeLabel(with: "upperLeft")
         let upperRightLabel = makeLabel(with: "upperRight")
         
+        let lowerLeftLabel = makeSecondaryLabel(withText: "lowerLeft")
+        let lowerRightButton = makeButton(withText: "Pay Bill")
+        
         view.addSubview(upperLeftLabel)
         view.addSubview(upperRightLabel)
+        view.addSubview(lowerLeftLabel)
+        view.addSubview(lowerRightButton)
         
         upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
         upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
         
         upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
         upperRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+        
+        lowerLeftLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 8).isActive = true
+        lowerLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        
+        lowerRightButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 8).isActive = true
+        lowerRightButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
     }
     
     // factory method
@@ -38,6 +49,26 @@ class ViewController: UIViewController {
         label.backgroundColor = .yellow
         
         return label
+    }
+    
+    func makeSecondaryLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .yellow
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .gray
+        
+        return label
+    }
+    
+    func makeButton(withText text: String) -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = .blue
+        
+        return button
     }
 
 }
