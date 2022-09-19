0dc9a273fc35acc603b7b76173bb281562f10b87
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-19 18:40:13 +0200

    using nslayout array to save on is active true space

--- AnchorsLab/AnchorsLab/SafeAreaViewController.swift
+++ AnchorsLab/AnchorsLab/SafeAreaViewController.swift
@@ -8,21 +8,29 @@
 import UIKit
 
 class SafeAreaViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
-       
+        
         setupViews()
     }
     
     func setupViews() {
         let topLabel = makeLabel(withText: "top")
+        let bottomLabel = makeLabel(withText: "bottom")
         
         view.addSubview(topLabel)
+        view.addSubview(bottomLabel)
         
-        topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            topLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            bottomLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            bottomLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        
     }
     
     func makeLabel(withText text: String) -> UILabel {
@@ -30,10 +38,11 @@ class SafeAreaViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = text
         label.backgroundColor = .yellow
+        label.font = UIFont.systemFont(ofSize: 32)
         
         return label
     }
     
-
+    
     
 }
