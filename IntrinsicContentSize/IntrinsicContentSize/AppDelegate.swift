ea3a4dfcf8b4a8b8154a42f625f346db8736258c
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-09-25 17:02:33 +0200

    adding intrinsic contentsize lab

---
+++ IntrinsicContentSize/IntrinsicContentSize/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  IntrinsicContentSize
+//
+//  Created by Mpilo Pillz on 2022/09/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+        
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.makeKeyAndVisible()
+            window?.backgroundColor = .orange
+
+            window?.rootViewController = ViewController()
+            
+            
+            return true
+        }
+
+
+}
+
