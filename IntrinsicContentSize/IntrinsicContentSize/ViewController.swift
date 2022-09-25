fed86f418b5d5b8baa296e70ac8e97f4a44eb405
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-09-25 17:13:41 +0200

    learning various ways to adjust height and width of label

--- IntrinsicContentSize/IntrinsicContentSize/ViewController.swift
+++ IntrinsicContentSize/IntrinsicContentSize/ViewController.swift
@@ -18,16 +18,31 @@ class ViewController: UIViewController {
     
     func setupViews() {
         let label1 = makeLabel(withText: "label1")
+        let label2 = makeBigLabel(withText: "bigLabel")
         
         view.addSubview(label1)
+        view.addSubview(label2
+        )
         
         label1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
         label1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        
+        label1.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        label1.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        NSLayoutConstraint.activate([
+            label2.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            label2.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8)
+        ])
+        
+        // another way to set the width
+//        label1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
         
     }
     
     func makeLabel(withText text: String) -> UILabel {
         let label = UILabel()
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = text
         label.font = UIFont.systemFont(ofSize: 32)
@@ -35,7 +50,24 @@ class ViewController: UIViewController {
         
         return label
     }
+    
+    func makeBigLabel(withText text: String) -> UILabel {
+        let label = BigLabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.backgroundColor = .green
+        
+        return label
+    }
 
 
+}
+
+class BigLabel: UILabel {
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 300)
+    }
 }
 
